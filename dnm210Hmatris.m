e5be78eae802131e0351141d2f427f92626415d3
Gursu <gursu.lnx.tr@gmail.com>
2016-11-26 23:01:06 +0200

    Add files via upload

---
+++ dnm210Hmatris.m
@@ -0,0 +1,231 @@
+%H matrisinin üretilmesi
+%H=[H1a,H1b;H2a,H2b;H3a,H3b;H4a,H4b;H5a,H5b]; yapýsýnda olacak.
+H1a=[];%H1a: E ölçümlerinin delta (açý) deðerlerine göre türevleri
+H1b=[];%H1b: E ölçümlerinin |V| (gerilim genliði) deðerlerine göre türevleri
+H2a=[];%H2a: P ölçümlerinin delta (açý) deðerlerine göre türevleri
+H2b=[];%H2b: P ölçümlerinin |V| (gerilim genliði) deðerlerine göre türevleri
+H3a=[];%H3a: Q ölçümlerinin delta (açý) deðerlerine göre türevleri
+H3b=[];%H3b: Q ölçümlerinin |V| (gerilim genliði) deðerlerine göre türevleri
+H4a=[];%H4a: Pij ölçümlerinin delta (açý) deðerlerine göre türevleri
+H4b=[];%H4b: Pij ölçümlerinin |V| (gerilim genliði) deðerlerine göre türevleri
+H5a=[];%H5a: Qij ölçümlerinin delta (açý) deðerlerine göre türevleri
+H5b=[];%H5b: Qij ölçümlerinin |V| (gerilim genliði) deðerlerine göre türevleri
+
+zsay=0;
+zif=[];
+
+%H1a
+for p=1:size(E,1)
+    zsay=zsay+1;    
+    k1=E(p,2);
+    
+    zif(zsay,1)=E(p,1)-V(k1);
+    
+    for q=2:N
+        H1a(p,q)=0;        
+    end
+end
+if size(H1a,1)>0
+    H1a=H1a(:,2:N);
+end
+
+%H1b
+for p=1:size(E,1)    
+    k1=E(p,2);
+    for q=1:N
+        if q==k1
+            H1b(p,q)=1;
+        else
+            H1b(p,q)=0;
+        end
+    end
+end
+
+%H2a
+for p=1:size(P,1)
+    zsay=zsay+1;
+    k1=P(p,2);
+    
+    zif(zsay,1)=P(p,1)-abs(V(k1))*abs(V(k1))*G(k1,k1);
+    for n=1:N
+        if n~=k1
+            zif(zsay,1)=zif(zsay,1)-abs(V(k1)*V(n)*Ybara(k1,n))*cos(teta(k1,n)+delta(n)-delta(k1));            
+        end        
+    end    
+    
+    for q=2:N
+        if q==k1
+            %i-i
+            H2a(p,q)=0;
+            for n=1:N
+                if n~=k1 
+                    H2a(p,q)=H2a(p,q)+abs(V(k1)*V(n)*Ybara(k1,n))*sin(teta(k1,n)+delta(n)-delta(k1));
+                end
+            end
+        else
+            %i-j
+            H2a(p,q)=-abs(V(k1)*V(q)*Ybara(k1,q))*sin(teta(k1,q)+delta(q)-delta(k1));
+        end        
+    end
+end
+if size(H2a,1)>0
+    H2a=H2a(:,2:N);
+end
+
+%H2b
+for p=1:size(P,1)
+    k1=P(p,2);
+    for q=1:N
+        if q==k1
+            %i-i
+            H2b(p,q)=2*abs(V(k1))*G(k1,k1);
+            for n=1:N
+                if n~=k1
+                    H2b(p,q)=H2b(p,q)+abs(V(n)*Ybara(k1,n))*cos(teta(k1,n)+delta(n)-delta(k1));
+                end                
+            end
+        else
+            %i-j
+            H2b(p,q)=abs(V(k1)*Ybara(k1,q))*cos(teta(k1,q)+delta(q)-delta(k1));
+        end
+    end
+end
+
+%H3a
+for p=1:size(Q,1)
+    zsay=zsay+1;    
+    k1=Q(p,2);    
+    
+    zif(zsay,1)=Q(p,1)+abs(V(k1))*abs(V(k1))*B(k1,k1);
+    for n=1:N
+        if n~=k1
+            zif(zsay,1)=zif(zsay,1)+abs(V(k1)*V(n)*Ybara(k1,n))*sin(teta(k1,n)+delta(n)-delta(k1));            
+        end        
+    end   
+    
+    for q=2:N
+        if q==k1
+            %i-i
+            H3a(p,q)=0;
+            for n=1:N
+                if n~=k1 
+                    H3a(p,q)=H3a(p,q)+abs(V(k1)*V(n)*Ybara(k1,n))*cos(teta(k1,n)+delta(n)-delta(k1));
+                end
+            end
+        else
+            %i-j
+            H3a(p,q)=-abs(V(k1)*V(q)*Ybara(k1,q))*cos(teta(k1,q)+delta(q)-delta(k1));
+        end        
+    end
+end
+if size(H3a,1)>0
+    H3a=H3a(:,2:N);
+end
+
+%H3b
+for p=1:size(Q,1)
+    k1=Q(p,2);
+    for q=1:N
+        if q==k1
+            %i-i
+            H3b(p,q)=-2*abs(V(k1))*B(k1,k1);
+            for n=1:N
+                if n~=k1
+                    H3b(p,q)=H3b(p,q)-abs(V(n)*Ybara(k1,n))*sin(teta(k1,n)+delta(n)-delta(k1));
+                end                
+            end
+        else
+            %i-j
+            H3b(p,q)=-abs(V(k1)*Ybara(k1,q))*sin(teta(k1,q)+delta(q)-delta(k1));
+        end
+    end
+end
+
+%H4a
+for p=1:size(Pij,1)
+    zsay=zsay+1;    
+    k1=Pij(p,2);
+    k2=Pij(p,3);
+    
+    zif(zsay,1)=Pij(p,1)-(-abs(V(k1))*abs(V(k1))*G(k1,k2)+abs(V(k1)*V(k2)*Ybara(k1,k2))*cos(teta(k1,k2)+delta(k2)-delta(k1)));   
+    
+    for q=2:N
+        if q==k1
+            %i-i
+            H4a(p,q)=abs(V(k1)*V(k2)*Ybara(k1,k2))*sin(teta(k1,k2)+delta(k2)-delta(k1));
+        else if q==k2
+                %i-j
+                H4a(p,q)=-abs(V(k1)*V(k2)*Ybara(k1,k2))*sin(teta(k1,k2)+delta(k2)-delta(k1));
+            else
+                H4a(p,q)=0;
+            end
+        end        
+    end
+end
+if size(H4a,1)>0
+    H4a=H4a(:,2:N);
+end
+    
+%H4b
+for p=1:size(Pij,1)
+    k1=Pij(p,2);
+    k2=Pij(p,3);
+    for q=1:N
+        if q==k1
+            %i-i
+            H4b(p,q)=-2*abs(V(k1))*G(k1,k2)+abs(V(k2)*Ybara(k1,k2))*cos(teta(k1,k2)+delta(k2)-delta(k1));
+        else if q==k2
+                %i-j
+                H4b(p,q)=abs(V(k1)*Ybara(k1,k2))*cos(teta(k1,k2)+delta(k2)-delta(k1));
+            else
+                H4b(p,q)=0;
+            end
+        end        
+    end
+end
+
+%H5a
+for p=1:size(Qij,1)
+    zsay=zsay+1;    
+    k1=Qij(p,2);
+    k2=Qij(p,3);
+    
+    zif(zsay,1)=Qij(p,1)-(-abs(V(k1))*abs(V(k1))*(Bkacak(k1,k2)/2-B(k1,k2))-abs(V(k1)*V(k2)*Ybara(k1,k2))*sin(teta(k1,k2)+delta(k2)-delta(k1)));   
+    
+    for q=2:N
+        if q==k1
+            %i-i
+            H5a(p,q)=abs(V(k1)*V(k2)*Ybara(k1,k2))*cos(teta(k1,k2)+delta(k2)-delta(k1));
+        else if q==k2
+                %i-j
+                H5a(p,q)=-abs(V(k1)*V(k2)*Ybara(k1,k2))*cos(teta(k1,k2)+delta(k2)-delta(k1));
+            else
+                H5a(p,q)=0;                
+            end
+        end        
+    end
+end
+if size(H5a,1)>0
+    H5a=H5a(:,2:N);
+end
+    
+%H5b
+for p=1:size(Qij,1)
+    k1=Qij(p,2);
+    k2=Qij(p,3);
+    for q=1:N
+        if q==k1
+            %i-i
+            H5b(p,q)=-2*abs(V(k1))*(Bkacak(k1,k2)/2-B(k1,k2))-abs(V(k2)*Ybara(k1,k2))*sin(teta(k1,k2)+delta(k2)-delta(k1));
+        else if q==k2
+                %i-j
+                H5b(p,q)=-abs(V(k1)*Ybara(k1,k2))*sin(teta(k1,k2)+delta(k2)-delta(k1));
+            else
+                H5b(p,q)=0;
+            end
+        end        
+    end
+end
+
+H=[H1a,H1b;H2a,H2b;H3a,H3b;H4a,H4b;H5a,H5b];
+%Ibrahim Gursu Tekdemir, 2016
---
+++ dnm210b.m
@@ -0,0 +1,155 @@
+function [Vgenlik,Vacid,dene]=dnm210b(skonum,yontem)
+%function [Vgenlik,Vacid,dene]=dnm210b(skonum,yontem)
+%
+%Vgenlik: Durum kestirimi sonrasında elde edilen gerilim genlik değeri (pu)
+%Vaci: Durum kestirimi sonrasında elde edilen gerilim açı değeri (radyan)
+%
+%skonum: Aşağıdaki ölçüm değerlerinin kaydedildiği .mat dosyasının konumu
+%konum: Değerleri okunacak olan sistemin txt dosyasının konumu
+%('sistem_elm.txt' gibi)
+%E: Kestirim öncesi bilinen gerilim genlik ölçümü değerleri (pu)
+%P: Kestirim öncesi bilinen bara aktif güç ölçümü değerleri (pu)
+%Q: Kestirim öncesi bilinen bara reaktif güç ölçümü değerleri (pu)
+%Pij: Kestirim öncesi bilinen hat aktif güç akışı ölçümü değerleri (pu)
+%Qij: Kestirim öncesi bilinen hat reaktif güç akışı ölçümü değerleri (pu)
+%Rx: Sırasıyla E, P, Q, Pij, Qij ölçümlerini yapan cihazların standart sapma
+%değerleri (Rx=[0.01,0.02,0.001]; %3 ölçüm için örnek bir Rx vektörü)
+%
+%ör1: E=[1 1;1.02 3]; %1. baranın ve 3. baranın gerilim genliği ölçümleri sırasıyla 1pu ve 1.02 pu değerindedir.
+%
+%ör2: P=[2.4 4;-3.2 5]; %4. baranın aktif güç ölçümü 2.4pu, 5. baranın
+%aktif güç ölçümü -3.2pu değerindedir. (Q değişkeni de aynı yapıdadır.)
+%
+%ör3: Pij=[1.2 1 5;-1.1 5 1]; %1. baradan 5. baraya aktif güç ölçümü 1.2pu,
+%5. baradan 1. baraya aktif güç ölçümü -1.1pu değerindedir. (Qij değişkeni de aynı yapıdadır.)
+%
+%Ibrahim Gursu Tekdemir, 2016
+
+load(skonum);
+
+%Yakınsama çözünürlüğü
+hata=0.001;
+
+%dx hesabına ilişkin yöntem seçimi
+%yontem=1;
+
+%Ybara matrisi, uygun txt dosyasından okunarak üretilecek.
+[Ybara,ypq2]=dosya_oku3(konum);
+Bkacak=abs(ypq2)*2;
+
+%G,B ve teta'ların Ybara'ya göre hesaplanması
+G=real(Ybara);
+B=imag(Ybara);
+teta=atan2(B,G);
+
+%Bara sayısı
+N=length(Ybara);
+
+%Toplam ölçüm sayısı
+Nm=size(E,1)+size(P,1)+size(Q,1)+size(Pij,1)+size(Qij,1);
+
+%R matrisinin üretilmesi
+for p=1:Nm
+    R(p,p)=1/((Rx(p))^2);
+end
+
+%Durumlar için başlangıç değerlerinin üretilmesi (initialization of states)
+for p=1:N
+    delta1(p)=0;
+    V1(p)=1;
+end
+
+dene=1;
+dongu=1;
+while dongu==1
+    %H matrisinde kullanılacak değerlerin elde edilmesi
+    delta(1)=0;
+    for n=2:N
+        delta(n)=delta1(dene,n);        
+    end
+    for n=1:N        
+        V(n)=V1(dene,n);
+    end
+    
+    %H matrisinin hesaplanması
+    dnm210Hmatris;
+        
+    %dx vektörünün elde edilmesi
+    if yontem==1
+        Ga=H'*(R\H);
+        dx=Ga\(H'*(R\zif));
+    else if yontem==2
+            Ga=H'*inv(R)*H;
+            %Yedek=inv(Ga)*H'*inv(R)*zif;
+            %dx=Ga\(H'*inv(R)*zif);
+            [ALT,UST,PER]=lu(Ga);
+            u=H'*inv(R)*zif;
+            u=PER*u;
+            
+            px=size(ALT,1);
+            qx=size(ALT,2);
+            
+            py=size(UST,1);
+            qy=size(UST,2);
+            
+            for p=1:px
+                sayac=0;
+                for q=1:qx
+                    if p==q t(p,1)=(u(p,1)-sayac)/ALT(p,q);
+                    else if ALT(p,q)==0 b=1;
+                        else sayac=sayac+ALT(p,q)*t(q,1);
+                        end
+                    end
+                end
+            end
+            
+            sat=py+1;
+            sut=qy+1;
+            for p=1:py
+                sayac=0;
+                sat=sat-1;
+                sut=qy+1;
+                for q=1:qy
+                    sut=sut-1;
+                    if sat==sut 
+                        %if UST(sat,sut)==0 dx(sat,1)=0; 
+                        %else
+                        dx(sat,1)=(t(sat,1)-sayac)/UST(sat,sut); 
+                        %end
+                    else if UST(sat,sut)==0 b=1;
+                        else sayac=sayac+UST(sat,sut)*dx(sut,1);
+                        end
+                    end
+                end
+            end           
+        end
+    end
+    
+    sayac=0;
+    delta1(dene+1,1)=0;
+    for n=2:N
+        sayac=sayac+1;
+        delta1(dene+1,n)=delta1(dene,n)+dx(sayac);       
+    end
+    for n=1:N
+        sayac=sayac+1;
+        V1(dene+1,n)=V1(dene,n)+dx(sayac);       
+    end
+       
+    enbuyuk=abs(dx(1,1));
+    for n=1:(2*N-1)
+        if abs(dx(n,1))>enbuyuk
+            enbuyuk=abs(dx(n,1));
+        end
+    end
+    if enbuyuk<hata || dene>50
+        dongu=0;
+    else
+        dene=dene+1;
+    end    
+end
+
+Vgenlik=V1(size(V1,1),:);
+Vacid=delta1(size(delta1,1),:)*180/pi;
+
+end
