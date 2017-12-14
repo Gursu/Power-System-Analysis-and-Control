3f1261f49d7defde07e560aec49ae68c414c1374
Ibrahim Gursu Tekdemir <gursu.lnx.tr@gmail.com>
2017-12-14 14:01:09 +0300

    Update DIdeneme.m

--- DIdeneme.m
+++ DIdeneme.m
@@ -1,3 +1,4 @@
+%A rough demonstration of Discrimination Index (DI) mentioned in article ( I. G. Tekdemir and B. Alboyaci, "A Novel Approach for Improvement of Power Swing Blocking and Deblocking Functions in Distance Relays," in IEEE Transactions on Power Delivery, vol. 32, no. 4, pp. 1986-1994, Aug. 2017. doi: 10.1109/TPWRD.2016.2600638 ) for a 5-cycle synthetically created sine wave. Amplitude of the wave is doubled after 2 cycles, so as to demonstrate a sudden change in system (a fault in a power system, e.g.).
 %clear;clc;
 t=0:0.001:0.1;
 y(1:40)=sin(2*pi*50*t(1:40));
@@ -11,4 +12,4 @@ for p=1:length(t)
 		end			
 	end
 end
-plot(t,y,t,10*DI);
+plot(t,y,t,10*DI);%DI is multiplied by 10 so as to make the view of graphics comfortable
